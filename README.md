2f02f8e4714fd5174c5a2cc645bf9eed27499137
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-09-05 20:01:56 -0500

    Updated model

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ docker-compose run web django-admin startproject composeexample .
 docker-compose up
 docker-compose down
 docker ps
+docker-compose run web python3 manage.py migrate
+docker-compose run web python3 manage.py makemigrations
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    bio = models.TextField()
+    birthdate = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
